61b73c07dc153b1aed7879974a714aeb1f8cd843
hasanbadr <hasan.badr@edu.keuda.fi>
2023-12-12 07:52:43 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static int Yhteenlasku(int a, int b)
+    {
+        return a + b;
+    }
+
+    static void Main()
+    {
+        Console.Write("Syötä ensimmäinen numero: ");
+        int numero1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Syötä toinen numero: ");
+        int numero2 = int.Parse(Console.ReadLine());
+
+        int tulos = Yhteenlasku(numero1, numero2);
+        Console.WriteLine($"Tulos: {tulos}");
+    }
+}
